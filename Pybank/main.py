fff49714ecf65e96fbaf067ddc599f0733f9925a
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-04-05 12:06:57 -0500

    First two excercises PyBank

--- Pybank/main.py
+++ Pybank/main.py
@@ -5,22 +5,38 @@ import csv
 
 # 1. Import csv file 
 
-csvpath = os.path.join('..', 'Resources_Pybank', 'Budget_data.csv')
+budget_csvpath = os.path.join('Resources_Pybank', 'Budget_data.csv')
 
-with open(csvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(budget_csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =",")
     print(csvreader)
 
 # Say which is the header 
-# 
+    csv_header = next (csvreader)
+    print (f"CSV Header: {csv_header}")
 # Create a Python script that analyzes the records to calculate each of the following:
     # The total number of months included in the dataset
         # 2.- Loop through everything to see which are the months 
-        # 3.- Print months 
-    # The net total amount of "Profit/Losses" over the entire period
+        # The net total amount of "Profit/Losses" over the entire period
         # 4.- Add everything.
         # 5.- Print result 
+    
+    months = [] 
+    profit = []
+    month_count = 0 
+    total_profit = 0
+    for row in csvreader:
+        month_count = month_count + 1
+        total_profit = int(row[1]) + total_profit
+    
 
+
+    
+    #print months 
+    print (month_count)
+    print(total_profit)
+
+    
     # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
         # 6.- With previous answer divide everything by the lenght of list (or download average funcion of statistics module)
     # The greatest increase in profits (date and amount) over the entire period
@@ -33,3 +49,5 @@ with open(csvpath) as csvfile:
         #12.- Do one number minut the next one 
         # 13. Find min result (use min.())
         # 14. Print both date and amount with min result
+    #Print the analysis in the terminal 
+    #Export file into a text file 
